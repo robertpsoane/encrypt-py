2a675f34efd9e59f249df4229eb9b02f0ea2aea7
robertpsoane <robertsoane101@gmail.com>
2020-10-11 08:40:41 +0100

    Added buttons and structure of function class

--- password_manager_gui.py
+++ password_manager_gui.py
@@ -1,30 +1,142 @@
 from programfiles.encrypt import RSAEncryptor as RSA
-from programfiles.buttons import *
+from programfiles.buttons import ButtonFunctions
 import tkinter as tk
 
-## Initiating Encryptor
+## Initiating Main Classes
 rsa = RSA()
+BF = ButtonFunctions(rsa)
+
+##### Important Variables
+screen_width = 640
+screen_height = 420
+screen_size = screen_width, screen_height
+background_colour = 'MOCCASIN'
 
 ##### Creating GUI
-gui = tk.Tk()
-gui.title('Password Manager')
-gui.configure(background='MOCCASIN')
+main_gui = tk.Tk()
+main_gui.title('Password Manager')
+main_gui.configure(background=background_colour)
 
+##### Setting Up Frames
+## Main Frame
+main_frame = tk.Frame(
+    main_gui, 
+    height = screen_height, 
+    width=screen_width,
+    background=background_colour).grid(row=0, column = 0)
+
+## Top Frame (Frame containing buttons and password tables)
+top_frame = tk.Frame(
+    main_frame,
+    height = (3/4) * screen_height,
+    width = screen_width,
+    background=background_colour
+    )
+
+## Bottom Frame (Frame containing path and key)
+bottom_frame = tk.Frame(
+    main_frame,
+    height = (1/4) * screen_height,
+    width = screen_width,
+    background=background_colour
+    )
+
+## Buttom Frame
+button_frame = tk.Frame(
+    top_frame,
+    height=(3/4) * screen_height,
+    width=(1/5)*screen_width,
+    background=background_colour
+    )
+
+## Password Table Frame
+table_frame = tk.Frame(
+    top_frame,
+    height = (3/4) * screen_width,
+    width = (4/5) * screen_width,
+    background=background_colour
+    )
+
+## Keys Frame
+keys_frame = tk.Frame(
+    bottom_frame,
+    height = (1/4) * screen_height,
+    width = screen_width,
+    background=background_colour,
+    )
+
+## Packing frames
+top_frame.pack_propagate(0) 
+top_frame.grid(row=0, column=0)
+
+bottom_frame.pack_propagate(0) 
+bottom_frame.grid(row=1, column=0)
+
+button_frame.pack_propagate(0) 
+button_frame.grid(row=0, column=0)
+
+table_frame.pack_propagate(0) 
+table_frame.grid(row=0, column=1)
+
+keys_frame.pack_propagate(0) 
+keys_frame.grid(row=0, column=0)
+
+##### Setting Up Buttons
 ## Creating Buttons
-new_record_button = tk.Button(gui, text='New File', command=newFile)
-load_record_button = tk.Button(gui, text='Load File', command=loadFile)
-add_record_button = tk.Button(gui, text='Add Record', command=addRecord)
-remove_record_button = tk.Button(gui, text='Remove Record', command=removeRecord)
-help_button = tk.Button(gui, text='Help', command=helpButton)
+new_record_button = tk.Button(
+    button_frame,
+    text='New File',
+    command=BF.newFile,
+    width = 15
+    )
 
+load_record_button = tk.Button(
+    button_frame,
+    text='Load File',
+    command=BF.loadFile,
+    width = 15
+    )
+
+add_record_button = tk.Button(
+    button_frame,
+    text='Add Record',
+    command=BF.addRecord,
+    width = 15
+    )
+
+remove_record_button = tk.Button(
+    button_frame,
+    text='Remove Record',
+    command=BF.removeRecord,
+    width = 15
+    )
+
+change_record_button = tk.Button(
+    button_frame,
+    text='Change Record',
+    command=BF.changeRecord,
+    width = 15
+    )
+
+help_button = tk.Button(
+    button_frame,
+    text='Help',
+    command=BF.helpButton,
+    width = 15
+    )
+
+## Packing Buttons
 buttons = [new_record_button,
     load_record_button,
     add_record_button, 
     remove_record_button,
+    change_record_button,
     help_button
     ]
 
-for button in buttons:
-    button.pack()
+for k in range(len(buttons)):
+    buttons[k].grid(row=k, column=0, pady = screen_height/50)
+
+
 
 tk.mainloop()
--- programfiles/buttons.py
+++ programfiles/buttons.py
@@ -2,22 +2,31 @@
 
 import webbrowser
 
-## New File Function
-def newFile():
-    pass
+class ButtonFunctions:
+    def __init__(self,encryptor):
+        self.encryptor = encryptor
+    
+    ## New File Function
+    def newFile(self):
+        pass
 
-## Load File Function
-def loadFile():
-    pass
+    ## Load File Function
+    def loadFile(self):
+        pass
 
-## New Record Function
-def addRecord():
-    pass
+    ## New Record Function
+    def addRecord(self):
+        pass
 
-## Remove Record Function
-def removeRecord():
-    pass
+    ## Remove Record Function
+    def removeRecord(self):
+        pass
 
-## Show Help Function
-def helpButton():
-    webbrowser.open("https://github.com/robertpsoane/encrypt-py")
+    ## Change Record Function
+    def changeRecord(self):
+        pass
+
+
+    ## Show Help Function
+    def helpButton(self):
+        webbrowser.open("https://github.com/robertpsoane/encrypt-py")
